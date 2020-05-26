446fb013d013ba43bf607e667c92c609e282c5b4
longvision <r.n.hori@gmail.com>
2020-05-26 13:10:41 -0300

    Updates

---
+++ precache-manifest.fb5e5f428ea5405149963e3b9ce7c607.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61472c45881d292627344652c5fd2ce2",
+    "url": "/frontend-reactjs-exam/index.html"
+  },
+  {
+    "revision": "05507a7133ea090295e0",
+    "url": "/frontend-reactjs-exam/static/css/2.be55d809.chunk.css"
+  },
+  {
+    "revision": "14dc02ad27b2c0ecac92",
+    "url": "/frontend-reactjs-exam/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "05507a7133ea090295e0",
+    "url": "/frontend-reactjs-exam/static/js/2.e712b442.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/frontend-reactjs-exam/static/js/2.e712b442.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14dc02ad27b2c0ecac92",
+    "url": "/frontend-reactjs-exam/static/js/main.0522072f.chunk.js"
+  },
+  {
+    "revision": "e3ed931b63ede4dc73b0",
+    "url": "/frontend-reactjs-exam/static/js/runtime-main.eaf5b250.js"
+  },
+  {
+    "revision": "a52b4e6bb7c6b01fed596d1c482643f9",
+    "url": "/frontend-reactjs-exam/static/media/check.a52b4e6b.svg"
+  },
+  {
+    "revision": "c46a900e812c6d5edc6a54071c95a344",
+    "url": "/frontend-reactjs-exam/static/media/demo.c46a900e.svg"
+  },
+  {
+    "revision": "063b7dcbf90979cecfa34192999ff718",
+    "url": "/frontend-reactjs-exam/static/media/logo.063b7dcb.svg"
+  },
+  {
+    "revision": "57af19a0560239931d7159a55be76262",
+    "url": "/frontend-reactjs-exam/static/media/novo-cartao.57af19a0.svg"
+  },
+  {
+    "revision": "dcd6dc8e910acccde86a4bbebfb9c5cd",
+    "url": "/frontend-reactjs-exam/static/media/side.dcd6dc8e.png"
+  }
+]);
